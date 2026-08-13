1fe56d6c6ed16d5f42ede81abee33c8f3829ccfe
Rasol07 <poloa0709@gmail.com>
2026-08-13 15:46:20 +0900

    feat: Pagination 기능 구현

--- sprint-mission-react/src/App.jsx
+++ sprint-mission-react/src/App.jsx
@@ -2,17 +2,21 @@ import { useEffect, useState } from "react";
 import { getProducts } from "./api/products";
 import "./App.css";
 
+const PAGE_PER_PRODUCTS = 5;
+
 function App() {
   // product 값 저장
   const [products, setProducts] = useState([]);
   const [totalCount, setTotalCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(totalCount / PAGE_PER_PRODUCTS);
 
   useEffect(() => {
     const productDataLoad = async () => {
       try {
         const { list, totalCount } = await getProducts({
-          page: 1,
-          pageSize: 4,
+          page: currentPage,
+          pageSize: PAGE_PER_PRODUCTS,
           standard: "favorite",
           keyword: "a",
         });
@@ -24,7 +28,15 @@ function App() {
     };
 
     productDataLoad();
-  }, []);
+  }, [currentPage]);
+
+  // page 이동
+  const goToPage = (pageNumber) => {
+    if (pageNumber <= 0 || pageNumber > totalPages) {
+      throw new Error("페이지 갯수가 맞지 않습니다.");
+    }
+    setCurrentPage(pageNumber);
+  };
 
   return (
     <div>
@@ -37,6 +49,18 @@ function App() {
           </li>
         ))}
       </ul>
+      <div>
+        <p>
+          현재 페이지 : {currentPage} / 전체 페이지 : {totalPages}
+          {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+            (pageNumber) => (
+              <button key={pageNumber} onClick={() => goToPage(pageNumber)}>
+                {pageNumber}
+              </button>
+            ),
+          )}
+        </p>
+      </div>
     </div>
   );
 }
